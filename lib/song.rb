8841463d62bdf3bbc53614750d10250665212c85
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-27 05:12:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,8 @@ class Song
 
   def initialize(name, artist, genre)
     @@count +=1
-    @@genres << genre 
+    @@genres << genre
+    @@artists << artist
     @name = name
     @artist = artist
     @genre = genre
