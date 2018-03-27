1bd4573eebfaa65a0461ece930b850fea3218832
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-27 05:04:51 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  
+  def initialize(name, artist, genre)
+    @@count +=1
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+end
